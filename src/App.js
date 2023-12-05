7bdab86c751191a34036091e29b9280830e65839
Joseph S <joseph.segarra@developersinstitute.co.nz>
2023-12-05 19:05:21 +1300

    fixing folder problems for deployment

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ import ArtGallery from "./components/ArtGallery";
 import CharacterInfo from "./components/CharacterInfo";
 import HelpPage from "./components/HelpPage";
 
-import RE2Remake from "./components/CharSheet/RE2R";
-import RE3Remake from "./components/CharSheet/RE3R";
-import RE4Remake from "./components/CharSheet/RE4R";
-import RE7 from "./components/CharSheet/RE7";
-import RE8 from "./components/CharSheet/RE8";
+import RE2Remake from "./components/RE2R";
+import RE3Remake from "./components/RE3R";
+import RE4Remake from "./components/RE4R";
+import RE7 from "./components/RE7";
+import RE8 from "./components/RE8";
 import Merchandise from "./components/Merchandise";
 
 function App() {
@@ -23,11 +23,11 @@ function App() {
         <Route path='/helppage' component={HelpPage} />
         <Route path='/character-info' component={CharacterInfo} />
 
-        <Route path='/CharSheet/RE2R' component={RE2Remake} />
-        <Route path='/CharSheet/RE3R' component={RE3Remake} />
-        <Route path='/CharSheet/RE4R' component={RE4Remake} />
-        <Route path='/CharSheet/RE7' component={RE7} />
-        <Route path='/CharSheet/RE8' component={RE8} />
+        <Route path='/RE2R' component={RE2Remake} />
+        <Route path='/RE3R' component={RE3Remake} />
+        <Route path='/RE4R' component={RE4Remake} />
+        <Route path='/RE7' component={RE7} />
+        <Route path='/RE8' component={RE8} />
       </div>
     </Router>
   );
--- src/components/RE7.jsx
+++ src/components/RE7.jsx
@@ -1,5 +1,5 @@
-import Header from "../Header";
-import Footer from "../Footer";
+import Header from "./Header";
+import Footer from "./Footer";
 import "./CSheet.css";
 
 const RE7 = () => {
